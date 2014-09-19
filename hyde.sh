2d29d614372a75ef8f824a5725cf54ea1308ff73
Frank Michael Sanchez <orion1701@me.com>
2014-09-19 11:00:15 -0400

    Initial Commit

---
+++ hyde.sh
@@ -0,0 +1,212 @@
+#! /bin/bash
+
+# Configuration #
+export JEKYLL_DIR="/Users/orion/developer/sites/fmscode.github.io/_posts"
+export MICRO_POST_LENGTH=240
+
+# Function: addPost
+# Parameters: $1: Filename
+#			  $2: Template name, Optional
+#			  $3: Post Title 
+# Description: Creates a new post in the _posts directory
+#
+addPost(){
+currentDate=`date +%Y-%m-%d`
+fullDateTime=`date +%Y-%m-%d' '%T`
+cat <<EOF > $(postsDirectory)/$currentDate-$1.md
+---
+layout: $2
+title: $3
+date: $fullDateTime
+---
+EOF
+}
+
+
+# Function: addMicroPost
+# Parameters: $1: FileName
+#			  $2: Content
+#			  $3: Link, Optional
+# Description: Function to add a new micro post
+#
+addMicroPost(){
+# Time/date for post
+currentDate=`date +%Y-%m-%d`
+fullDateTime=`date +%Y-%m-%d' '%T`
+# Basic post content
+postContent="
+---
+layout: post
+title: $1
+date: $fullDateTime
+tags: micropost
+---
+$2"
+# Check if a link was passed with this post
+if [ ${#3} -ne 0 ]
+then
+postContent="$postContent 
+
+[$3]($3)"
+fi
+# Create file
+cat <<EOF > $(postsDirectory)/$currentDate-$1.md
+	$postContent
+EOF
+}
+
+# Function: checkPostDirectory
+# Description: Checks if the current location contains the _posts Jekyll directory
+#
+checkPostsDirectory(){
+	if [ ${#JEKYLL_DIR} -ne 0 ]; then
+		if [ ! -d "$JEKYLL_DIR" ]; then
+			echo "Invalid Jekyll Posts location"
+			exit;
+		fi
+	else
+		# Check if _posts directory exists.
+		if [ ! -d "${PWD}/_posts" ]; then
+			echo "$(tput setaf 1)Posts directory was not found!$(tput sgr0) Please make sure you are in the Jekyll directory."
+			exit;
+		fi
+	fi
+}
+
+# Function: postsDirectory
+# Description: Returns the Jekyll _posts directory or whichever directory used for JEKYLL_DIR.
+#
+postsDirectory(){
+	if [ ${#JEKYLL_DIR} -ne 0 ]; then
+		echo $JEKYLL_DIR
+	else
+		echo "${PWD}/_posts"
+	fi
+}
+
+# Function: scriptHelp
+# Parameters: $1: Action command, Optional
+# Description: The help output with action support
+#
+scriptHelp(){
+	case $1 in
+		"post" | "add" )
+			cat <<-EndAddHelp
+			Action: post|add
+			Options:
+				  -f
+				    Post Title
+				  -t
+				    Post Layout
+
+			EndAddHelp
+		;;
+		"micro" )
+			cat <<-EndMicorHelp
+			Action: micro
+			Usage: micro [content] [options]
+			Options:
+				  content
+				    The content of the micro post.
+				  -l
+				    A micropost link
+			EndMicorHelp
+		;;
+		* )
+			cat <<-EndHelp
+			  Usage: hyde [action] [options]
+
+			  Actions:
+				    add|a "A new post to be added."
+				    micro "A new micropost"
+			  
+			  'hyde [action] -h' for help on the action
+			EndHelp
+		;;
+	esac
+	exit 0
+}
+
+# Begin Script
+# Read in the action from the user
+action=$1
+# Shift to the options parameters
+shift 1
+# Logic for each action
+case $action in
+	"post" | "add" )
+		checkPostsDirectory
+		# Read in the options that the add Action supports
+		while getopts "f:t:" Option
+			do
+				case $Option in
+					'f' )
+						# Post title is given
+						postTitle=$OPTARG
+						# File name is the post title without any space characters
+						fileName=`echo "$postTitle" | tr ' ' '-'`
+					;;
+					't' )
+						# Defines the layout the post to use, used for Jekyll templates
+						layout=$OPTARG
+					;;
+					'h' )
+
+					;;
+				esac
+		done
+		# Check post title length
+		if [ ${#postTitle} == 0 ]; then
+			echo "Invalid post title"
+			exit;
+		fi
+		# Set default for the layout if layout is not set
+		if [ ${#layout} == 0 ]; then
+			layout=post
+		fi
+		# Create post file
+		addPost "$fileName" "$layout" "$postTitle"
+	;;
+	"micro" )
+		checkPostsDirectory
+		# Use current time as post title
+		cTime=$(date +"%T")
+		cTime=`echo "$cTime" | tr -d ':'`
+		fileName="$cTime"
+		# Check the length of the post to be MICRO_POST_LENGTH
+		postContent=$1
+		postContentLength=${#postContent}
+		if [ $postContentLength -eq 0 ]; then
+			echo "$(tput setaf 1)Invalid post!$(tput sgr0)"
+		else
+			if [ $postContentLength -gt $MICRO_POST_LENGTH ]; then
+				# Ouput the post and show the user where 240 characters end.
+				echo "$(tput setaf 1)Invalid post length$(tput sgr0)"
+				echo "${postContent:0:240}$(tput setaf 1)${postContent:240}$(tput sgr0)"
+				exit;
+			else
+				shift
+				# Read in the options that the micro Action supports
+				while getopts "l:" Option
+					do
+						case $Option in
+							'l' )
+								# Post title is given
+								link=$OPTARG
+							;;
+						esac
+				done
+				# Create micro post
+				addMicroPost "$fileName" "$postContent" "$link"
+			fi
+		fi
+
+	;;
+	"help" )
+		scriptHelp "$1"
+	;;
+	* )
+		echo "Invalid Command"
+	;;
+esac
+
